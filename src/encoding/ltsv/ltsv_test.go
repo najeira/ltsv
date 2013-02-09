b75e1864716d4b4c160e3acb898d36afcc6697b5
najeira <najeira@gmail.com>
2013-02-09 14:36:31 +0900

    first commit

---
+++ src/encoding/ltsv/ltsv_test.go
@@ -0,0 +1,64 @@
+package ltsv
+
+import (
+	"bytes"
+	"testing"
+	"io"
+)
+
+type readerTest struct {
+	value  string
+	records []map[string]string
+}
+
+var readerTests = []readerTest {
+	{
+		`host:127.0.0.1	ident:-	user:frank	time:[10/Oct/2000:13:55:36 -0700]	req:GET /apache_pb.gif
+
+HTTP/1.0	status:200	size:2326	referer:http://www.example.com/start.html	ua:Mozilla/4.08 [en] (Win98; I ;Nav)
+`,
+		[]map[string]string{
+			{"host": "127.0.0.1", "ident": "-", "user": "frank", "time": "[10/Oct/2000:13:55:36 -0700]", "req": "GET /apache_pb.gif"},
+			{"status": "200", "size": "2326", "referer": "http://www.example.com/start.html", "ua": "Mozilla/4.08 [en] (Win98; I ;Nav)"},
+		},
+	},
+	{
+		` trimspace :こんにちは
+		 trim space :こんばんは
+日本語:ラベル
+ha,s.p-un_ct: おはよう `,
+		[]map[string]string{
+			{"trimspace": "こんにちは"},
+			{"trim space": "こんばんは"},
+			{"日本語": "ラベル"},
+			{"ha,s.p-un_ct": " おはよう "},
+		},
+	},
+}
+
+func TestReader(t *testing.T) {
+	for n, test := range readerTests {
+		reader := NewReader(bytes.NewBufferString(test.value))
+		for i, result := range test.records {
+			record, err := reader.Read()
+			if err != nil {
+				t.Errorf("error %v at test %d, line %d", err, n, i)
+			}
+			for label, field := range result {
+				if record[label] != field {
+					t.Errorf("wrong field %s: test %d, line %d, label %s, field %s", record[label], n, i, label, field)
+				}
+			}
+			if len(result) != len(record) {
+				t.Errorf("wrong size %d, %v :test %d, line %d", len(record), record, n, i)
+			}
+		}
+		_, err := reader.Read()
+		if err != io.EOF {
+			t.Errorf("expected EOF: %v", err)
+		}
+	}
+}
+
+func TestWriter(t *testing.T) {
+}
---
+++ src/encoding/ltsv/reader.go
@@ -0,0 +1,152 @@
+// based on encoding/csv
+package ltsv
+
+import (
+	"bufio"
+	"bytes"
+	"errors"
+	"fmt"
+	"io"
+	"unicode"
+	"strings"
+)
+
+// A Reader reads records from a LTSV-encoded file.
+//
+// As returned by NewReader, a Reader expects input LTSV-encoded file.
+// The exported fields can be changed to customize the details before the
+// first call to Read or ReadAll.
+//
+// Delimiter is the field delimiter.  It defaults to '\t'.
+//
+// Comment, if not 0, is the comment character. Lines beginning with the
+// Comment character are ignored.
+type Reader struct {
+	Delimiter rune // Field delimiter (set to '\t' by NewReader)
+	Comment   rune // Comment character for start of line
+	line      int
+	r         *bufio.Reader
+	label     bytes.Buffer
+	field     bytes.Buffer
+}
+
+// NewReader returns a new Reader that reads from r.
+func NewReader(r io.Reader) *Reader {
+	return &Reader{
+		Delimiter: '\t',
+		r:         bufio.NewReader(r),
+	}
+}
+
+// Read reads one record from r.  The record is a slice of strings with each
+// string representing one field.
+func (r *Reader) Read() (map[string]string, error) {
+	r.line++
+	record := make(map[string]string)
+	for {
+		r1, err := r.readRune()
+		if r1 == '\n' {
+			continue // skip empty line
+		}
+		
+		// If we are support comments and it is the comment character
+		// then skip to the end of line.
+		if r.Comment != 0 && r1 == r.Comment {
+			for {
+				r1, err := r.readRune()
+				if err != nil {
+					return nil, err
+				} else if r1 == '\n' {
+					break
+				}
+			}
+			continue
+		}
+		r.r.UnreadRune()
+		
+		label, err := r.parseLabel()
+		if err != nil {
+			return nil, err
+		}
+		if label == "" {
+			continue // skip empty label
+		}
+		
+		field, end, err := r.parseField()
+		if err != nil {
+			return nil, err
+		}
+		
+		record[label] = field
+		if end {
+			return record, nil
+		}
+	}
+	panic("unreachable")
+}
+
+func (r *Reader) parseLabel() (string, error) {
+	r.label.Reset()
+	for {
+		r1, err := r.readRune()
+		if err != nil {
+			return "", err
+		} else if r1 == ':' {
+			return strings.TrimSpace(r.label.String()), nil
+		} else if r1 == '\t' {
+			return "", nil // no label
+		} else if unicode.IsControl(r1) || !unicode.IsPrint(r1) {
+			return "", errors.New(fmt.Sprintf("line %d: invalid rune at label", r.line))
+		}
+		r.label.WriteRune(r1)
+	}
+	panic("unreachable")
+}
+
+func (r *Reader) parseField() (string, bool, error) {
+	r.field.Reset()
+	for {
+		r1, err := r.readRune()
+		if err != nil {
+			if err == io.EOF {
+				return r.field.String(), true, nil
+			}
+			return "", false, err
+		} else if r1 == '\t' {
+			return r.field.String(), false, nil
+		} else if r1 == '\n' {
+			return r.field.String(), true, nil
+		}
+		r.field.WriteRune(r1)
+	}
+	panic("unreachable")
+}
+
+func (r *Reader) readRune() (rune, error) {
+	r1, _, err := r.r.ReadRune()
+	if r1 == '\r' {
+		r1, _, err = r.r.ReadRune()
+		if err == nil {
+			if r1 != '\n' {
+				r.r.UnreadRune()
+				r1 = '\r'
+			}
+		}
+	}
+	return r1, err
+}
+
+func (r *Reader) ReadAll() ([]map[string]string, error) {
+	records := make([]map[string]string, 0)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			return records, nil
+		}
+		if err != nil {
+			return nil, err
+		}
+		records = append(records, record)
+	}
+	panic("unreachable")
+}
